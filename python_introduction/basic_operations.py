806787f9e5bc41d97f4f5ac403742c621ece62b2
richeasy001 <tenebe.osikhena@gmail.com>
2024-09-15 09:48:00 +0100

    basic ops file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+Add = number1 + number2
+Sub = number1 - number2
+mult = number1 * number2
+
+print ('Addition of', number1, 'and', number2, 'is', Add)
+print ('Subtraction of', number1, 'and', number2, 'is', Sub)
+print ('Multiplication of', number1, 'and', number2, 'is', mult)
